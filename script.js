30fe0bd394c878b0d1e4c9cf2f53fdd727e645d0
FCristanchoBOCC <fcristancho@bancodeoccidente.com.co>
2024-02-04 16:45:10 -0500

    Add articles

--- script.js
+++ script.js
@@ -6,3 +6,14 @@
             $("#modal").addClass("hidden");
         });
     });
+
+    
+// Menu mobile
+    document.addEventListener("DOMContentLoaded", function () {
+        var menuButton = document.querySelector("[data-collapse-toggle='navbar-default']");
+        var menuList = document.getElementById("navbar-default");
+    
+        menuButton.addEventListener("click", function () {
+          menuList.classList.toggle("hidden");
+        });
+      });
